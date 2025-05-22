392231f50624bf6811032d3ace0a4968ee6e570e
Rafael <rafamed.tech@gmail.com>
2025-05-22 15:58:56 -0700

    Update image sources in AppCarousel component

--- layers/base/app/components/AppCarousel.vue
+++ layers/base/app/components/AppCarousel.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 const items = [
-  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747952441/Screenshot_2025-05-22_at_3.20.31_p.m._uk1lm5.png',
-  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747950729/tv_dv3rnt.jpg',
-  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747952441/Screenshot_2025-05-22_at_3.20.31_p.m._uk1lm5.png',
-  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747950729/tv_dv3rnt.jpg',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747954693/olivoweb_amkcv5.jpg',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747954693/oivoweb2_rdgzei.jpg',
+  'https://res.cloudinary.com/rafamed-dev/image/upload/v1747954693/OLIVOWEB3_akia1i.jpg',
 ];
 </script>
 
